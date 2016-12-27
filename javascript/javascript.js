3dd4243afbd0a887dc8abc6f4b9c53ed673a6ccb
Tyler <tcameronsmith@gmail.com>
2016-12-27 15:20:02 -0500

    add callback that adds class for list tag in nav-bar that underlines current page user is on

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -7,3 +7,17 @@ $(document).ready(function(){
   });
 
 });
+
+$('header').ready(function() {
+  var page = window.location.href.split("/")[7]; // after the slash at the end of the domain name
+  if (page === "index.html")
+    $(".index").addClass("current-page");
+  else if (page === "portfolio.html")
+    $(".portfolio").addClass("current-page");
+  else if (page === "resume.html")
+    $(".resume").addClass("current-page");
+  else if (page === "about.html")
+    $(".about").addClass("current-page");
+  else
+    $(".contact").addClass("current-page");
+});
